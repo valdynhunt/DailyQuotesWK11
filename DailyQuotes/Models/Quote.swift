48a3336956e35d09ace529b92af4ad81d0f13794
Valdyn Hunt <valdyn@mac.com>
2023-04-29 10:12:09 -0700

    amend quote model to accomodate different API

--- DailyQuotes/Models/Quote.swift
+++ DailyQuotes/Models/Quote.swift
@@ -33,20 +33,19 @@
 import Foundation
 
 struct Quote: Codable, Identifiable {
-  let id: String
-  let author: String?
-  let text: String?
+    let id = UUID()
+    let author: String?
+    let text: String?
 
-  enum CodingKeys: String, CodingKey {
-    case id, author
-    case text = "en"
-  }
+    enum CodingKeys: String, CodingKey {
+        case author
+        case text = "quote"
+    }
 
-  static var example: Quote {
-    Quote(
-      id: "asd875sad68s6ad",
-      author: "Steve Jobs",
-      text: "The people who are crazy enough to think they can change the world are the ones who do."
-    )
-  }
+    static var example: Quote {
+        Quote(
+            author: "Steve Jobs",
+            text: "The people who are crazy enough to think they can change the world are the ones who do."
+        )
+    }
 }
